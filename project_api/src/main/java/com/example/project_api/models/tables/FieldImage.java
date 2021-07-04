e1f6f3e474efd2d23949b4622ab4b87bec0c7d54
chayveng <chayveng@outlook.com>
2021-07-04 20:33:08 +0700

    removate api java 11i

---
+++ project_api/src/main/java/com/example/project_api/models/tables/FieldImage.java
@@ -0,0 +1,34 @@
+package com.example.project_api.models.tables;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "field_image")
+public class FieldImage implements Comparable<FieldImage>{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private long fieldId;
+
+    @Column
+    private String fileName;
+
+    @Column
+    @Lob
+    private byte[] file;
+
+    @Override
+    public int compareTo(FieldImage o) {
+        return this.fileName.compareTo(o.fileName);
+    }
+
+}
