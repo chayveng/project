5c639184717a8fb73921bc6f633bf295fea45f88
chaluemwuut <chaluemwut@hotmail.com>
2021-08-23 11:45:29 +0700

    เพิ่ม image_path parameter ของ env

--- project_app/lib/core/Config.dart
+++ project_app/lib/core/Config.dart
@@ -1,7 +1,6 @@
-
 class Config {
   // static String API_URL = 'http://localhost:80';
   // static String API_URL = 'http://192.168.1.114:80';
-  static String API_URL = 'http://172.16.3.37:80';
+  static String API_URL = 'http://10.0.2.2';
   // static String API_URL = 'http://35.185.188.64:80';
 }
