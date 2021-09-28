6a5cfb19dec68ae504305db194e96af7de5e241d
chayveng <chayveng@outlook.com>
2021-09-29 00:10:17 +0700

    form srn branch

--- project_app/lib/core/Config.dart
+++ project_app/lib/core/Config.dart
@@ -1,8 +1,8 @@
 class Config {
-  static String API_URL = 'http://localhost:80';
+  // static String API_URL = 'http://localhost:80';
   // static String API_URL = 'http://192.168.1.114:80';
   // static String API_URL = 'http://10.0.2.2';
   // static String API_URL = 'http://35.185.188.64:80';
   // static String API_URL = 'https://fitbuddy.cc/soccerhub';
-  // static String API_URL = 'http://localhost:8080/soccerhub';
+  static String API_URL = 'http://localhost:8080/soccerhub';
 }
